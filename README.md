e35e0f240f4a0f37f2590d799f86c1fcb9b942eb
杨旗 <yangq06@mysoft.com.cn>
2020-06-08 20:46:02 +0800

    add jest

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # swagger-typescript-gen
+参考
+https://github.com/mtennoe/swagger-typescript-codegen
+https://github.com/wcandillon/swagger-js-codegen
+https://github.com/areijngoudt/swagger-ts-generator
--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,8 @@ interface CodeGenOptions {
 }
 
 export const getTypescriptCode = function (opts: CodeGenOptions) {
-
+  if (opts.swagger.swagger !== '2.0') {
+    throw Error('Only Swagger 2.0 specs are supported')
+  }
+  return true
 }
