ccf1bf8dcdf1f1bd695d76337b25de98e6c6f3d9
杨旗 <yangq06@mysoft.com.cn>
2020-07-19 18:05:45 +0800

    update beautijs

--- src/enhance/beautify.ts
+++ src/enhance/beautify.ts
@@ -1,6 +1,34 @@
 import { js_beautify } from "js-beautify";
 import { defaults } from "lodash";
 
+export interface JsBeautifyOptions {
+  indent_size?: number;
+  indent_char?: string;
+  eol?: string;
+  indent_level?: number;
+  indent_with_tabs?: boolean;
+  preserve_newlines?: boolean;
+  max_preserve_newlines?: number;
+  jslint_happy?: boolean;
+  space_after_anon_function?: boolean;
+  brace_style?:
+    | "collapse-preserve-inline"
+    | "collapse"
+    | "expand"
+    | "end-expand"
+    | "none";
+  keep_array_indentation?: boolean;
+  keep_function_indentation?: boolean;
+  space_before_conditional?: boolean;
+  break_chained_methods?: boolean;
+  eval_code?: boolean;
+  unescape_strings?: boolean;
+  wrap_line_length?: number;
+  wrap_attributes?: "auto" | "force";
+  wrap_attributes_indent_size?: number;
+  end_with_newline?: boolean;
+}
+
 const DEFAULT_BEAUTIFY_OPTIONS: JsBeautifyOptions = {
   indent_size: 4,
   max_preserve_newlines: 2
